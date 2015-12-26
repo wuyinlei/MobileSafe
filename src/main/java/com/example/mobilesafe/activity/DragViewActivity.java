c0948b2023f6a85b56fdd6dbc000a5fdb668e477
wuyinlei <1069584784@qq.com>
2015-12-26 13:16:33 +0800

    完成自定义悬浮框的位置显示和提示的交互，加入了半透明的效果，在网上查看了activity和appcompatactivity的主题，调整了translucent的主题适用于appcompatactivity

--- src/main/java/com/example/mobilesafe/activity/DragViewActivity.java
+++ src/main/java/com/example/mobilesafe/activity/DragViewActivity.java
@@ -1,5 +1,6 @@
 package com.example.mobilesafe.activity;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +10,16 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mobilesafe.R;
 
+import java.lang.reflect.Field;
+
 public class DragViewActivity extends AppCompatActivity {
 
+    //sbar为状态栏的高度
+    int x = 0, sbar = 0;
     private int startX, startY;
     private TextView tvTop;
     private TextView tvBottom;
@@ -45,10 +51,24 @@ public class DragViewActivity extends AppCompatActivity {
         int lastX = mPres.getInt("lastX", 0);
         int lastY = mPres.getInt("lastY", 0);
 
+
         //初始化布局的步奏
         //onMeasure(测量view)   onLayout(放置的位置)   onDraw(绘制)
         // ivdrag.layout(lastX,lastY,lastX+ivdrag.getWidth(),lastY+ivdrag.getHeight());
         // 不能用此方法，因为我们还没有测量完成就不能安放位置
+
+        //获取屏幕的宽高，这个包含了状态栏的高度
+        final int winWidth = getWindowManager().getDefaultDisplay().getWidth();
+        final int winHeight = getWindowManager().getDefaultDisplay().getHeight();
+
+        //根据图片的位置来设置提示框的显示位置
+        if (lastY > winHeight/2){   //上边的显示，下部的隐藏
+            tvTop.setVisibility(View.VISIBLE);
+            tvBottom.setVisibility(View.INVISIBLE);
+        } else {
+            tvTop.setVisibility(View.INVISIBLE);
+            tvBottom.setVisibility(View.VISIBLE);
+        }
 
         //拿到布局的参数   加载子控件的。
         //父标签是谁，下面的LayoutParams之前的就是谁
@@ -83,6 +103,39 @@ public class DragViewActivity extends AppCompatActivity {
                         int r = ivdrag.getRight() + dx;
                         int t = ivdrag.getTop() + dy;
                         int b = ivdrag.getBottom() + dy;
+
+
+                        /**
+                         * 在这里通过反射的机制来获取到状态栏的高度，
+                         * 同理还可以获取到actionBar的高度
+                         */
+                        Class<?> aClass = null;
+                        Object obj = null;
+                        Field field = null;
+                        try {
+                            aClass = Class.forName("com.android.internal.R$dimen");
+                            obj = aClass.newInstance();
+                            field = aClass.getField("status_bar_height");
+                            x = Integer.parseInt(field.get(obj).toString());
+                            sbar = getResources().getDimensionPixelSize(x);
+                        } catch(Exception e1) {
+                            e1.printStackTrace();
+                        }
+
+                        //如果满足了一下的情况，就直接break，一下的情况是
+                        //出左侧边界，出上部边界，出下部边界，出右侧边界
+                        if (l<0 || r>winWidth || t<0 || b>winHeight-sbar){
+                            break;
+                        }
+
+                        //根据图片的位置来设置提示框的显示位置
+                        if (t > winHeight/2){   //上边的显示，下部的隐藏
+                            tvTop.setVisibility(View.VISIBLE);
+                            tvBottom.setVisibility(View.INVISIBLE);
+                        } else {
+                            tvTop.setVisibility(View.INVISIBLE);
+                            tvBottom.setVisibility(View.VISIBLE);
+                        }
                         //更新界面
                         /**
                          * @param l Left position, relative to parent
