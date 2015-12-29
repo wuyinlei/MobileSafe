c801ca933acd01a204297eeb61f0d42f23a3fe08
wuyinlei <1069584784@qq.com>
2015-12-29 20:30:52 +0800

    加入电话拦截功能和删除拦截的电话的通话记录

--- src/main/java/com/example/mobilesafe/service/CallSafeService.java
+++ src/main/java/com/example/mobilesafe/service/CallSafeService.java
@@ -2,18 +2,30 @@ package com.example.mobilesafe.service;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 
+import com.android.internal.telephony.ITelephony;
 import com.example.mobilesafe.db.BlackNumberDao;
+
+import java.lang.reflect.Method;
 
 public class CallSafeService extends Service {
 
     private InnerReceive receive;
     private BlackNumberDao dao;
+    private TelephonyManager tm;
+    private MyPhoneStateListener listener;
+    private Class<?> clazz;
 
 
     public CallSafeService() {
@@ -30,10 +42,100 @@ public class CallSafeService extends Service {
         receive = new InnerReceive();
 
         dao = new BlackNumberDao(this);
+
+        //获取到系统的电话服务
+        tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+
+        listener = new MyPhoneStateListener();
+        tm.listen(listener,Integer.MAX_VALUE);
+
         //注册了短信的广播，初始化
         IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
         filter.setPriority(Integer.MAX_VALUE);
         registerReceiver(receive, filter);
+    }
+
+    class MyPhoneStateListener extends PhoneStateListener{
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+            switch (state){
+                case TelephonyManager.CALL_STATE_RINGING:
+                    //电话铃响
+
+                    String mode = dao.findNumber(incomingNumber);
+                    if (mode.equals("1") || mode.equals("2")){
+
+                        //得到uri
+                        Uri uri= Uri.parse("content://call_log/calls");
+                        getContentResolver().registerContentObserver(uri,true,new MyContentObserver(new Handler(),incomingNumber));
+
+                        //挂断电话
+                        endCall();
+                    }
+
+                    break;
+
+            }
+        }
+    }
+
+    private class MyContentObserver extends ContentObserver{
+
+        String incomingNumber;
+
+        /**
+         * Creates a content observer.
+         *
+         * @param handler The handler to run {@link #onChange} on, or null if none.
+         */
+        public MyContentObserver(Handler handler,String incomingNumber) {
+            super(handler);
+            this.incomingNumber = incomingNumber;
+        }
+
+
+        //当数据改变的额时候调用此方法
+        @Override
+        public void onChange(boolean selfChange) {
+            
+            getContentResolver().unregisterContentObserver(this);
+            deleteCallNumber(incomingNumber);
+            super.onChange(selfChange);
+        }
+    }
+
+    /**
+     * 挂断电话删除电话号码
+     * @param incomingNumber
+     */
+    private void deleteCallNumber(String incomingNumber) {
+        Uri uri= Uri.parse("content://call_log/calls");
+        getContentResolver().delete(uri,"number=?",new String[]{incomingNumber});
+    }
+
+
+    /**
+     * 挂断电话
+     */
+    private void endCall() {
+
+        try {
+            //通过类加载器加载ServiceManager
+            clazz = getClassLoader().loadClass("android.os.ServiceManager");
+            //通过反射得到当前的方法
+            Method method = clazz.getDeclaredMethod("getService", String.class);
+
+            IBinder iBinder = (IBinder) method.invoke(null, TELECOM_SERVICE);
+
+            ITelephony iTelephony = ITelephony.Stub.asInterface(iBinder);
+
+            iTelephony.endCall();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private class InnerReceive extends BroadcastReceiver {
